9f4f4c42d8089d8adb43c2cb97e46cd5edb37d8a
Deverlex <nguyendo94vn@gmail.com>
2017-12-18 11:23:17 +0700

    add icon for bottom bar.

--- app/src/main/java/vn/needy/buyer/screen/main/MainActivity.java
+++ app/src/main/java/vn/needy/buyer/screen/main/MainActivity.java
@@ -1,8 +1,22 @@
 package vn.needy.buyer.screen.main;
 
+import android.os.Bundle;
+
+import com.roughike.bottombar.BottomBar;
+
+import vn.needy.buyer.R;
+import vn.needy.buyer.screen.BaseActivity;
+
 /**
  * Created by lion on 16/12/2017.
  */
 
-public class MainActivity {
+public class MainActivity extends BaseActivity {
+
+    private BottomBar mBottomBar;
+
+    @Override
+    protected void onCreateActivity(Bundle savedInstanceState) {
+        setContentView(R.layout.activity_main);
+    }
 }
