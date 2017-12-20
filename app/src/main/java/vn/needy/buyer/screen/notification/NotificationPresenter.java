ecd4bee0bc2139daf6f0fdd650b0b382f9d50ab4
Pham Quang Truong <truongphamit@gmail.com>
2017-12-20 16:58:23 +0700

    design notification and fake data

--- app/src/main/java/vn/needy/buyer/screen/notification/NotificationPresenter.java
+++ app/src/main/java/vn/needy/buyer/screen/notification/NotificationPresenter.java
@@ -1,8 +1,55 @@
 package vn.needy.buyer.screen.notification;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import vn.needy.buyer.model.Notification;
+
 /**
  * Created by lion on 16/12/2017.
  */
 
-public class NotificationPresenter {
+public class NotificationPresenter implements NotificationContract.Presenter{
+
+    private NotificationContract.ViewModel mViewModel;
+
+    public NotificationPresenter(NotificationContract.ViewModel mViewModel) {
+        this.mViewModel = mViewModel;
+    }
+
+    @Override
+    public void onStart() {
+
+    }
+
+    @Override
+    public void onStop() {
+
+    }
+
+    @Override
+    public void onGetNotification() {
+        List<Notification> notifications = new ArrayList<>();
+        for (int i = 1; i <= 10; i++) {
+            Notification notification = new Notification();
+            notification.setId(i);
+            notification.setSenderId("senderId");
+            notification.setCreateTime("09:21 19-12-17");
+            notification.setTitle("Khuyến mãi hấp dẫn");
+            notification.setHtmlContent("<font color=\"red\">Mua một tặng một </font> cho tất cả các sản phẩm gas<br/><font color=\"red\">Miễn phí vẫn chuyển</font>  cho đơn hàng từ 500k trở lên");
+            notification.setReferenceGUI(".screen.main.MainActivity");
+            notifications.add(notification);
+            Random random = new Random();
+            notification.setIsReaded(random.nextBoolean());
+        }
+        mViewModel.onSetNotifications(notifications);
+    }
+
+    @Override
+    public void onReadAll(List<Notification> notifications) {
+        for (Notification notification : notifications) {
+            notification.setIsReaded(true);
+        }
+    }
 }
--- app/src/main/java/vn/needy/buyer/screen/notification/NotificationViewModel.java
+++ app/src/main/java/vn/needy/buyer/screen/notification/NotificationViewModel.java
@@ -1,9 +1,74 @@
 package vn.needy.buyer.screen.notification;
 
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.databinding.BaseObservable;
+import android.databinding.Bindable;
+
+import java.util.List;
+
+import vn.needy.buyer.BR;
+import vn.needy.buyer.model.Notification;
+import vn.needy.buyer.screen.BaseRecyclerViewAdapter;
+
 /**
  * Created by lion on 16/12/2017.
  */
 
-public class NotificationViewModel {
+public class NotificationViewModel extends BaseObservable implements NotificationContract.ViewModel,
+        BaseRecyclerViewAdapter.OnRecyclerViewItemClickListener<Object>{
 
+    private NotificationContract.Presenter mPresenter;
+
+    private Context mContext;
+    private NotificationAdapter mAdapter;
+
+    public NotificationViewModel(Context mContext, NotificationAdapter adapter) {
+        this.mContext = mContext;
+        mAdapter = adapter;
+        adapter.setItemClickListener(this);
+    }
+
+    @Override
+    public void onStart() {
+        mPresenter.onGetNotification();
+    }
+
+    @Override
+    public void onStop() {
+
+    }
+
+    @Override
+    public void setPresenter(NotificationContract.Presenter presenter) {
+        mPresenter = presenter;
+    }
+
+    @Bindable
+    public NotificationAdapter getAdapter() {
+        return mAdapter;
+    }
+
+    @Override
+    public void onSetNotifications(List<Notification> notifications) {
+        mAdapter.setData(notifications);
+    }
+
+    @Override
+    public void onClickReadAll() {
+        mPresenter.onReadAll(mAdapter.getData());
+        notifyPropertyChanged(BR.adapter);
+    }
+
+    @Override
+    public void onItemRecyclerViewClick(Object item) {
+        Notification notification = (Notification) item;
+        notification.setIsReaded(true);
+        mAdapter.notifyItemChanged(mAdapter.getPosition(notification));
+
+        Intent intent=new Intent();
+        intent.setComponent(new ComponentName(mContext.getPackageName(), mContext.getPackageName() + notification.getReferenceGUI()));
+        mContext.startActivity(intent);
+    }
 }
