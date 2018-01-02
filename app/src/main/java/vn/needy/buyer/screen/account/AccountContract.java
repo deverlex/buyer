92fd6a2466323e98db29e12bb949c30a28670d62
minh dai <haminhdai1997@gmail.com>
2018-01-02 11:10:04 +0700

    refactor code : account

--- app/src/main/java/vn/needy/buyer/screen/account/AccountContract.java
+++ app/src/main/java/vn/needy/buyer/screen/account/AccountContract.java
@@ -1,9 +1,46 @@
 package vn.needy.buyer.screen.account;
 
+import vn.needy.buyer.screen.BasePresenter;
+import vn.needy.buyer.screen.BaseViewModel;
+
 /**
  * Created by lion on 16/12/2017.
  */
 
 interface AccountContract {
+
+    interface ViewModel extends BaseViewModel<Presenter> {
+
+        void onUserProfileClicked();
+
+        void onDeleteAccountClicked();
+
+        void onClearCacheClicked();
+
+        void onReviewApplicationClicked();
+
+        void onBackPressed();
+
+        void onPersonalSettingClicked();
+
+        void onNotificationSettingClicked();
+
+        void onLanguageSettingClicked();
+
+        void onSupportClicked();
+
+        void onTipAndTrickClicked();
+
+        void onCommunityRulesClicked();
+
+        void onNeedRulesClicked();
+
+        void onLogout();
+
+    }
+
+    interface Presenter extends BasePresenter {
+
+    }
 
 }
--- app/src/main/java/vn/needy/buyer/screen/account/AccountPresenter.java
+++ app/src/main/java/vn/needy/buyer/screen/account/AccountPresenter.java
@@ -1,8 +1,26 @@
 package vn.needy.buyer.screen.account;
 
+import android.content.Context;
+
 /**
  * Created by lion on 16/12/2017.
  */
 
-public class AccountPresenter {
+public class AccountPresenter implements AccountContract.Presenter {
+
+    private Context mContext;
+
+    public AccountPresenter(Context mContext) {
+        this.mContext = mContext;
+    }
+
+    @Override
+    public void onStart() {
+
+    }
+
+    @Override
+    public void onStop() {
+
+    }
 }
