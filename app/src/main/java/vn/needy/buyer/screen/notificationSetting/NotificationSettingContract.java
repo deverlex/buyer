57413b7824d5d99fcd42dbfeef2c361eb863ea30
minh dai <haminhdai1997@gmail.com>
2018-01-02 11:12:26 +0700

    create notification setting

---
+++ app/src/main/java/vn/needy/buyer/screen/notificationSetting/NotificationSettingContract.java
@@ -0,0 +1,55 @@
+package vn.needy.buyer.screen.notificationSetting;
+
+import vn.needy.buyer.screen.BasePresenter;
+import vn.needy.buyer.screen.BaseViewModel;
+
+/**
+ * Created by lion on 25/12/2017.
+ */
+
+interface NotificationSettingContract {
+
+    interface ViewModel extends BaseViewModel<Presenter> {
+
+        void onNotificationCLick();
+
+        void onNotificationEmailCLick();
+
+        void onBackPressed();
+
+        void onNotification();
+
+        void onNotificationSettingEmail();
+
+        void onNotificationSetting();
+
+        void onNotificationUpdateProductClick();
+
+        void onNotificationChatClick();
+
+        void onNotificationProductPromotionClick();
+
+        void onNotificationFollowCLick();
+
+        void onNotificationProductEndCLick();
+
+        void onNotificationUpdateAppClick();
+
+        void onNotificationProductClick();
+
+        void notificationEmailApp();
+
+        void notificationNewClick();
+
+        void onNotificationPersonalClick();
+
+        void onNotificationGroupClick();
+
+        void onNotificationSave();
+    }
+
+    interface Presenter extends BasePresenter {
+
+    }
+
+}
