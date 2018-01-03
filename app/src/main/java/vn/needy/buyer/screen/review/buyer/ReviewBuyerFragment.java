46f3250fbaff3f4b032ae9a6e72cb1d8da765e63
Deverlex <nguyendo94vn@gmail.com>
2018-01-03 11:25:51 +0700

    add new files architecture for review functions.

---
+++ app/src/main/java/vn/needy/buyer/screen/review/buyer/ReviewBuyerFragment.java
@@ -0,0 +1,8 @@
+package vn.needy.buyer.screen.review.buyer;
+
+/**
+ * Created by lion on 03/01/2018.
+ */
+
+public class ReviewBuyerFragment {
+}
---
+++ app/src/main/java/vn/needy/buyer/screen/review/vendor/ReviewVendorFragment.java
@@ -0,0 +1,8 @@
+package vn.needy.buyer.screen.review.vendor;
+
+/**
+ * Created by lion on 03/01/2018.
+ */
+
+public class ReviewVendorFragment {
+}
