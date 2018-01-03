998838ede6ff6a1571476d5ba19b24b8ca59a366
Pham Quang Truong <truongphamit@gmail.com>
2018-01-03 10:44:52 +0700

    show list products pl of store

---
+++ app/src/main/java/vn/needy/buyer/screen/listProduct/priceLater/ItemProductPlViewModel.java
@@ -0,0 +1,25 @@
+package vn.needy.buyer.screen.listProduct.priceLater;
+
+import android.databinding.BaseObservable;
+
+import vn.needy.buyer.port.wrapper.ProductPlWrapper;
+import vn.needy.buyer.screen.BaseRecyclerViewAdapter;
+
+/**
+ * Created by truongpq on 03/01/2018.
+ */
+
+public class ItemProductPlViewModel extends BaseObservable {
+    private ProductPlWrapper mProductPl;
+    private BaseRecyclerViewAdapter.OnRecyclerViewItemClickListener<Object> mItemClickListener;
+
+    public ItemProductPlViewModel(ProductPlWrapper productPl, BaseRecyclerViewAdapter.OnRecyclerViewItemClickListener<Object> itemClickListener) {
+        this.mProductPl = productPl;
+        this.mItemClickListener = itemClickListener;
+    }
+    public void onItemClick() {
+        if (mItemClickListener != null) {
+            mItemClickListener.onItemRecyclerViewClick(mProductPl);
+        }
+    }
+}
---
+++ app/src/main/java/vn/needy/buyer/screen/listProduct/priceLater/ProductPlAdapter.java
@@ -0,0 +1,74 @@
+package vn.needy.buyer.screen.listProduct.priceLater;
+
+import android.content.Context;
+import android.databinding.DataBindingUtil;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+import java.util.List;
+
+import vn.needy.buyer.R;
+import vn.needy.buyer.databinding.ItemProductPlBinding;
+import vn.needy.buyer.port.wrapper.ProductPlWrapper;
+import vn.needy.buyer.screen.BaseRecyclerViewAdapter;
+
+/**
+ * Created by truongpq on 03/01/2018.
+ */
+
+public class ProductPlAdapter extends BaseRecyclerViewAdapter<ProductPlAdapter.ItemViewHolder>{
+
+    private BaseRecyclerViewAdapter.OnRecyclerViewItemClickListener<Object> mItemClickListener;
+    private List<ProductPlWrapper> mProductPl;
+
+    protected ProductPlAdapter(@NonNull Context context, List<ProductPlWrapper> productPlWrappers) {
+        super(context);
+        this.mProductPl = productPlWrappers;
+    }
+
+    @Override
+    public ItemViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        ItemProductPlBinding binding = DataBindingUtil.inflate(LayoutInflater.from(parent.getContext()),
+                R.layout.item_product_pl, parent, false);
+        return new ItemViewHolder(binding, mItemClickListener);
+    }
+
+    @Override
+    public void onBindViewHolder(ItemViewHolder holder, int position) {
+        holder.bind(mProductPl.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return mProductPl.size();
+    }
+
+    public void setItemClickListener(OnRecyclerViewItemClickListener<Object> itemClickListener) {
+        this.mItemClickListener = itemClickListener;
+    }
+
+    public void setData(List<ProductPlWrapper> productPlWrappers) {
+        mProductPl.clear();
+        mProductPl.addAll(productPlWrappers);
+        notifyDataSetChanged();
+    }
+
+    static class ItemViewHolder extends RecyclerView.ViewHolder {
+
+        private final ItemProductPlBinding mBinding;
+        private BaseRecyclerViewAdapter.OnRecyclerViewItemClickListener<Object> mItemClickListener;
+
+        public ItemViewHolder(ItemProductPlBinding binding, BaseRecyclerViewAdapter.OnRecyclerViewItemClickListener<Object> itemClickListener) {
+            super(binding.getRoot());
+            this.mBinding = binding;
+            this.mItemClickListener = itemClickListener;
+        }
+
+        void bind(ProductPlWrapper productPlWrapper) {
+            mBinding.setViewModel(new ItemProductPlViewModel(productPlWrapper, mItemClickListener));
+            mBinding.executePendingBindings();
+        }
+    }
+}
