9fd864f7171ec81354dff837efb38e9ddbd67935
minh dai <haminhdai1997@gmail.com>
2018-01-09 10:51:54 +0700

    create StoreFavoriteActivity

---
+++ app/src/main/java/vn/needy/buyer/screen/storeFavorite/StoreFavoriteViewModel.java
@@ -0,0 +1,25 @@
+package vn.needy.buyer.screen.storeFavorite;
+
+import android.databinding.BaseObservable;
+
+/**
+ * Created by minh_dai on 09/01/2018.
+ */
+
+public class StoreFavoriteViewModel extends BaseObservable implements StoreFavoriteContract.ViewMode{
+
+    @Override
+    public void onStart() {
+
+    }
+
+    @Override
+    public void onStop() {
+
+    }
+
+    @Override
+    public void setPresenter(StoreFavoriteContract.Presenter presenter) {
+
+    }
+}
